944f9ff4d7cf36aadf24580a5d16b172f12b00c9
vrossi96 <vrossi396@yahoo.it>
2022-01-27 15:42:09 +0100

    add arrows col and img col

---
+++ js/script.js
@@ -0,0 +1,23 @@
+Descrizione:
+/* 
+Partendo dal markup fornito in allegato, implementare la logica per far funzionare lo slider:
+Deve essere possibile scorrere le immagini cliccando sulle freccette.
+I pallini in basso devono illuminarsi in base all'immagine che sto visualizzando.
+Far si che cliccando i pallini, venga selezionata l'immagine corrispondente.
+Note:  scegliamo una delle due seguenti soluzioni per le freccette:
+Se stiamo visualizzando la prima immagine, la freccetta di sinistra può:
+scomparire
+passare all'ultima immagine
+Se stiamo visualizzando l'ultima immagine, la freccetta  a destra può:
+scomparire
+passare alla prima immagine
+Siate coerenti: o scompaiono o continuano a navigare, no a soluzioni miste!!
+Bonus:
+1- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+2- quando il mouse va in hover sulle freccette o sui pallini l'autoplay si interrompe
+3 - l'autoplay riparte quando il mouse lascia le freccette o i pallini
+ */
+
+const app = new Vue({
+   el: "#root",
+});
