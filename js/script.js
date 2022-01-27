7d65259356a22846362d3acd75e9f9ac818a84eb
vrossi96 <vrossi396@yahoo.it>
2022-01-27 17:48:28 +0100

    fix left arrow

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
 /* 
-Partendo dal markup fornito in allegato, implementare la logica per far funzionare lo slider:
-Deve essere possibile scorrere le immagini cliccando sulle freccette.
-I pallini in basso devono illuminarsi in base all'immagine che sto visualizzando.
-Far si che cliccando i pallini, venga selezionata l'immagine corrispondente.
-Note:  scegliamo una delle due seguenti soluzioni per le freccette:
-Se stiamo visualizzando la prima immagine, la freccetta di sinistra può:
-scomparire
-passare all'ultima immagine
-Se stiamo visualizzando l'ultima immagine, la freccetta  a destra può:
-scomparire
-passare alla prima immagine
+|Partendo dal markup fornito in allegato, implementare la logica per far funzionare lo slider:
+|Deve essere possibile scorrere le immagini cliccando sulle freccette.
+|I pallini in basso devono illuminarsi in base all'immagine che sto visualizzando.
+|Far si che cliccando i pallini, venga selezionata l'immagine corrispondente.
+|Note:  scegliamo una delle due seguenti soluzioni per le freccette:
+|Se stiamo visualizzando la prima immagine, la freccetta di sinistra può:
+|scomparire passare all'ultima immagine
+|Se stiamo visualizzando l'ultima immagine, la freccetta  a destra può: scomparire passare alla prima immagine
 Siate coerenti: o scompaiono o continuano a navigare, no a soluzioni miste!!
 Bonus:
 1- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
@@ -59,7 +56,7 @@ const app = new Vue({
       },
       prevImg() {
          if (this.actualImg === 0) {
-            this.actualImg = this.images.length;
+            this.actualImg = this.images.length - 1;
          } else {
             this.actualImg--;
          }
